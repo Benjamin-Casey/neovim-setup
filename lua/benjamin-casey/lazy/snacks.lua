fdc82a1665db20b5e60bc8111fa348e02ce3082f
Benjamin Casey <benjamincasey55@gmail.com>
2025-02-20 17:23:58 +1000

    Added snacks?

--- lua/benjamin-casey/lazy/snacks.lua
+++ lua/benjamin-casey/lazy/snacks.lua
@@ -5,6 +5,7 @@ return {
 		scratch = { enabled = true },
 		indent = { enabled = true },
 	},
+
 	keys = {
     { "<leader>z",  function() Snacks.zen() end, desc = "Toggle Zen Mode" },
     { "<leader>Z",  function() Snacks.zen.zoom() end, desc = "Toggle Zoom" },
