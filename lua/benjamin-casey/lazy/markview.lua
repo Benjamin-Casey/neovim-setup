0979b33de798eb192e4c054cbfbfd0c1b697172d
Benjamin Casey <benjamincasey55@gmail.com>
2025-02-25 01:27:02 +1000

    New plugin - markview; previews markdownf iles

---
+++ lua/benjamin-casey/lazy/markview.lua
@@ -0,0 +1,8 @@
+-- For `plugins/markview.lua` users.
+return {
+  "OXY2DEV/markview.nvim",
+  lazy = false,
+  keys = {
+    { "<leader>mt", "<cmd>Markview toggle<cr>", desc = "Markview toggle" },
+  }
+}
