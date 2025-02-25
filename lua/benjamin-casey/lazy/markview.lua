e383a9a6485c6ff0720c4b5c5d68881e020841d2
Benjamin Casey <benjamincasey55@gmail.com>
2025-02-25 21:23:23 +1000

    Removed markview

--- lua/benjamin-casey/lazy/markview.lua
+++
@@ -1,8 +0,0 @@
--- For `plugins/markview.lua` users.
-return {
-  "OXY2DEV/markview.nvim",
-  lazy = false,
-  keys = {
-    { "<leader>mt", "<cmd>Markview toggle<cr>", desc = "Markview toggle" },
-  }
-}
