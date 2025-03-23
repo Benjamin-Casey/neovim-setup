c232a59a4f7ba8c75c96b90fdfed4a0247af7e98
Benjamin Casey <benjamincasey55@gmail.com>
2025-03-23 18:41:45 +1000

    Added telescope-file-browser

--- lua/v2/lazy/telescope.lua
+++ lua/v2/lazy/telescope.lua
@@ -16,7 +16,7 @@ return {
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {
 			desc = "Telescope live grep",
 		})
-		vim.keymap.set("n", "<leader>fb", builtin.buffers, {
+		vim.keymap.set("n", "<leader>fB", builtin.buffers, {
 			desc = "Telescope buffers",
 		})
 		vim.keymap.set("n", "<leader>fh", builtin.help_tags, {
--- lua/v2/remap.lua
+++ lua/v2/remap.lua
@@ -47,7 +47,7 @@ vim.keymap.set("n", "<leader>tN", "<cmd>tabp<CR>", { desc = "Go to previous tab"
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Move current buffer to new tab" }) --  move current buffer to new tab
 
 -- File explorer toggle - TODO
-vim.keymap.set("n", "<leader>e", "<cmd>lua MiniFiles.open()<cr>", { desc = "Toggles miniFiles" })
+vim.keymap.set("n", "<leader>E", "<cmd>lua MiniFiles.open()<cr>", { desc = "Toggles miniFiles" })
 
 -- No highlight
 vim.keymap.set("n", "<leader>nh", "<cmd>noh<cr>", { desc = "No highlight" })
