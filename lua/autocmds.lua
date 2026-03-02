34be1c7e1dfa4a99941f4584f5e33d3a9a2c9856
Benjamin Casey <benjamincasey55@gmail.com>
2026-03-02 15:52:37 +1000

    removed yank reset autocmd and added tiny-diagnostics plugin

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -29,44 +29,44 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Keep the cursor position when yanking
-local cursorPreYank
-
-vim.keymap.set({ "n", "x" }, "y", function()
-  cursorPreYank = vim.api.nvim_win_get_cursor(0)
-  return "y"
-end, { expr = true })
-
-vim.keymap.set("n", "Y", function()
-  cursorPreYank = vim.api.nvim_win_get_cursor(0)
-  return "y$"
-end, { expr = true })
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    if vim.v.event.operator == "y" and cursorPreYank then
-      vim.api.nvim_win_set_cursor(0, cursorPreYank)
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "WinScrolled" }, {
-  desc = "Fix scrolloff when you are at the EOF",
-  group = vim.api.nvim_create_augroup("ScrollEOF", { clear = true }),
-  callback = function()
-    if vim.api.nvim_win_get_config(0).relative ~= "" then
-      return -- Ignore floating windows
-    end
-
-    local win_height = vim.fn.winheight(0)
-    local scrolloff = math.min(vim.o.scrolloff, math.floor(win_height / 2))
-    local visual_distance_to_eof = win_height - vim.fn.winline()
-
-    if visual_distance_to_eof < scrolloff then
-      local win_view = vim.fn.winsaveview()
-      vim.fn.winrestview({ topline = win_view.topline + scrolloff - visual_distance_to_eof })
-    end
-  end,
-})
+-- local cursorPreYank
+--
+-- vim.keymap.set({ "n", "x" }, "y", function()
+--   cursorPreYank = vim.api.nvim_win_get_cursor(0)
+--   return "y"
+-- end, { expr = true })
+--
+-- vim.keymap.set("n", "Y", function()
+--   cursorPreYank = vim.api.nvim_win_get_cursor(0)
+--   return "y$"
+-- end, { expr = true })
+--
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+--   callback = function()
+--     if vim.v.event.operator == "y" and cursorPreYank then
+--       vim.api.nvim_win_set_cursor(0, cursorPreYank)
+--     end
+--   end,
+-- })
+--
+-- vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI", "WinScrolled" }, {
+--   desc = "Fix scrolloff when you are at the EOF",
+--   group = vim.api.nvim_create_augroup("ScrollEOF", { clear = true }),
+--   callback = function()
+--     if vim.api.nvim_win_get_config(0).relative ~= "" then
+--       return -- Ignore floating windows
+--     end
+--
+--     local win_height = vim.fn.winheight(0)
+--     local scrolloff = math.min(vim.o.scrolloff, math.floor(win_height / 2))
+--     local visual_distance_to_eof = win_height - vim.fn.winline()
+--
+--     if visual_distance_to_eof < scrolloff then
+--       local win_view = vim.fn.winsaveview()
+--       vim.fn.winrestview({ topline = win_view.topline + scrolloff - visual_distance_to_eof })
+--     end
+--   end,
+-- })
 
 -- Automatically Split help Buffers to the right
 vim.api.nvim_create_autocmd("FileType", {
