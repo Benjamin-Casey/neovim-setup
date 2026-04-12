eeda88627238b73cec50154d45a78f597d069ae0
Benjamin Casey <benjamincasey55@gmail.com>
2026-04-12 19:12:31 +1000

    Ensure treesitter starts on buffer entry

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,16 +1,27 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	lazy = false,
-	build = ":TSUpdate",
-	opts = {
-		-- Install parsers synchronously (only applied to `ensure_installed`)
-		sync_install = false,
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  opts = {
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-		-- Automatically install missing parsers when entering buffer
-		-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-		auto_install = true,
-		highlight = {
-			enabled = true,
-		},
-	},
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
+    highlight = {
+      enabled = true,
+    },
+  },
+  config = function()
+    vim.api.nvim_create_autocmd("BufReadPost", {
+      pattern = "*",
+      callback = function()
+        -- can start a specific treesitter on a specific buffer also
+        -- vim.treesitter.start(0, "c")
+        vim.treesitter.start()
+      end,
+      once = true,
+    })
+  end
 }
